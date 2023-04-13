3deb5682b0436bebb6a38db06bc4b7e6336323f3
Eduardo Parra <eduparra90@gmail.com>
2023-04-13 02:20:30 +0200

    improvements

---
+++ src/config/menu.ts
@@ -0,0 +1,16 @@
+export const SECTIONS = [
+	{title: 'Historia', href: '/historia', description: 'Conoce nuestra historia'},
+	{title: 'Pueblo', href: '/pueblo', description: 'Descubre los rincones de nuestro pueblo'},
+	{title: 'San Lorenzo', href: '/san-lorenzo', description: 'Toda la información sobre la hermandad de San Lorenzo' },
+	{title: 'Ocio', href: '/ocio', description: 'Te mostramos algunas de las actividades que puedes realizar en nuestro pueblo' },
+	{title: 'Comercios', href: '/comercios', description: 'Comercios en el pueblo o con reparto a domicilio en él' },
+	{title: 'Alojamiento', href: '/alojamiento', description: '¿Quieres visitarnos? Aquí puedes quedarte' },
+	{title: 'En venta', href: '/en-venta', description: 'Casas, chalets y terrenos en venta'},
+	{title: 'Contacto', href: '/contacto', description: ''},
+]
+
+
+export const MENU = [
+	{title: 'Inicio', href: '/'},
+	...SECTIONS
+]
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#094293',
+				'secondary': '#4c6da6',
+				"indigo": {
+					50: "#DDEBFD",
+					100: "#BCD6FB",
+					200: "#78ADF7",
+					300: "#3584F3",
+					400: "#0D60D3",
+					500: "#094293",
+					600: "#073473",
+					700: "#052757",
+					800: "#041A3A",
+					900: "#020D1D",
+					950: "#01070E"
+				}
+			}
+		},
 	},
 	plugins: [],
 }
