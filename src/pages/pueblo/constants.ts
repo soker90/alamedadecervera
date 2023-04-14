cd070c2a3f7ccc1763e5646a47919c1d23cc8af1
Eduardo Parra <edue17@gmail.com>
2023-04-14 09:33:58 +0200

    Update constants.ts

--- src/pages/pueblo/constants.ts
+++ src/pages/pueblo/constants.ts
@@ -20,7 +20,7 @@ export const PLACES = [
 		image: "/images/parque.jpg",
 		map: "https://goo.gl/maps/TqhojU7CVaypS3jj8",
 		web: null,
-		body: "Es el parque más grande, ubicado a la entrada de la localidad, es el parque más grande. En su entrada se " +
+		body: "Es el parque más grande, ubicado a la entrada de la localidad. En su entrada se " +
 			"encuentra el puente del Gran Prior y dentro de él está la piscina municipal y su terraza. Dispone de un área " +
 			"de juegos infantiles con diversos columpios y toboganes."
 	}, {
