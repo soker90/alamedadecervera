a32ac9f77ca4dea093853549ac3b6e8c01c29333
Eduardo Parra <eduparra90@gmail.com>
2023-04-19 23:07:14 +0200

    improvements

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -3,33 +3,39 @@ export const SECTIONS = [
 		title: 'Historia',
 		href: '/historia',
 		description:
-			'Conoce algunos acontecimientos históricos que ocurrieron aquí'
+			'Conoce algunos acontecimientos históricos que ocurrieron aquí',
+		logo: '/icons/history.svg'
 	},
 	{
 		title: 'Pueblo',
 		href: '/pueblo',
-		description: 'Descubre que puedes visitar y que servicios ofrece el pueblo'
+		description: 'Descubre que puedes visitar y que servicios ofrece el pueblo',
+		logo: '/icons/village.svg'
 	},
 	{
 		title: 'San Lorenzo',
 		href: '/san-lorenzo',
-		description: 'Toda la información sobre la hermandad de San Lorenzo'
+		description: 'Toda la información sobre la hermandad de San Lorenzo',
+		logo: '/icons/san-lorenzo.svg'
 	},
 	{
 		title: 'Actividades',
 		href: '/actividades',
 		description:
-			'Te mostramos algunas de las actividades que puedes realizar en nuestro pueblo'
+			'Te mostramos algunas de las actividades que puedes realizar en nuestro pueblo',
+		logo: '/icons/activity.svg'
 	},
 	{
 		title: 'Comercios',
 		href: '/comercios',
-		description: 'Comercios en el pueblo o con reparto a domicilio en él'
+		description: 'Comercios en el pueblo o con reparto a domicilio en él',
+		logo: '/icons/shop.svg'
 	},
 	{
 		title: 'Alojamiento',
 		href: '/alojamiento',
-		description: '¿Quieres visitarnos? Echa un vistazo a donde puedes alojarte'
+		description: '¿Quieres visitarnos? Echa un vistazo a donde puedes alojarte',
+		logo: '/icons/bed.svg'
 	}
 	// {title: 'En venta', href: '/en-venta', description: 'Casas, chalets y terrenos en venta'},
 	// {
