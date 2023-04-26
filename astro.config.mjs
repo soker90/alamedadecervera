157025f97410c2eb81dda2ac919a148e383521ee
Eduardo Parra <eduparra90@gmail.com>
2023-04-26 21:20:15 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import tailwind from "@astrojs/tailwind";
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable Preact to support Preact JSX components.
-  integrations: [preact(), tailwind(), mdx()]
-});
+	site: 'https://www.alamedadecervera.com',
+	// Enable Preact to support Preact JSX components.
+	integrations: [preact(), tailwind(), mdx(), sitemap()]
+})
