85a0364c2538c9d3fb1b62e9c818033589df2019
Eduardo Parra <edue17@gmail.com>
2023-05-20 01:06:32 +0200

    integration with vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-
 import image from '@astrojs/image'
+
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,19 @@ export default defineConfig({
 	// Enable Preact to support Preact JSX components.
 	integrations: [preact(), tailwind(), mdx(), sitemap(), image({
 		// Sharp is not supported for my computer
-		...(import.meta.env.MODE !== 'development' && { serviceEntryPoint: '@astrojs/image/sharp' })
-	})]
+		...(import.meta.env.MODE !== 'development' && {
+			serviceEntryPoint: '@astrojs/image/sharp'
+		})
+	})],
+	output: 'static',
+	adapter: vercel({
+		analytics: true,
+		imageConfig: {
+			sizes: [320, 640, 1280]
+		},
+		imageService: true
+	}),
+	experimental: {
+		assets: true
+	}
 })
