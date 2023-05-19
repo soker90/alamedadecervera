41d0c4d161fab9429048ebd7a1e409ceb489601b
Eduardo Parra <edue17@gmail.com>
2023-05-20 00:31:52 +0200

    Optimize images with sharp (#4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-import image from "@astrojs/image";
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alamedadecervera.com',
-  // Enable Preact to support Preact JSX components.
-  integrations: [preact(), tailwind(), mdx(), sitemap(), image()]
-});
+	site: 'https://alamedadecervera.com',
+	// Enable Preact to support Preact JSX components.
+	integrations: [preact(), tailwind(), mdx(), sitemap(), image({
+		// Sharp is not supported for my computer
+		...(import.meta.env.MODE !== 'development' && { serviceEntryPoint: '@astrojs/image/sharp' })
+	})]
+})
