78d1ad31198f41eaf6d630f89e482117d0f39e73
Eduardo Parra <edue17@gmail.com>
2023-05-20 00:09:36 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-import image from "@astrojs/image";
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alamedadecervera.com',
-  // Enable Preact to support Preact JSX components.
-  integrations: [preact(), tailwind(), mdx(), sitemap(), image()]
-});
+	site: 'https://alamedadecervera.com',
+	// Enable Preact to support Preact JSX components.
+	integrations: [preact(), tailwind(), mdx(), sitemap(), image({
+		serviceEntryPoint: '@astrojs/image/sharp'
+	})]
+})
