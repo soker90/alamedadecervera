390a53483882af7ca5db77508137316a9e7cb68d
Eduardo Parra <edue17@gmail.com>
2023-05-20 19:38:37 +0200

    fix images pueblo

--- src/pages/pueblo/constants.ts
+++ src/pages/pueblo/constants.ts
@@ -4,8 +4,8 @@ export const PLACES = [
 		places: [
 			{
 				title: 'Puente del Gran Prior',
-				image: '@assets/images/puente.webp',
-				maps: ['https://goo.gl/maps/o7t4ZY5oV1JrhXHo9'],
+				image: '/images/puente.webp',
+				maps: [ 'https://goo.gl/maps/o7t4ZY5oV1JrhXHo9'],
 				body: 'Fue mandado construir por Juan de Villanueva sobre un antiguo canal a finales de 1797 y concluye a finales de 1799.\n' +
 					'Posee un haz de 3,40 metros, y una altura sobre el lecho de 3,20 metros. Lo más singular de este puente, son las\n' +
 					'gradas que enmarcan su embocadura y descienden hasta el lecho del canal, con un desarrollo radial a modo de escalera\n' +
@@ -13,8 +13,8 @@ export const PLACES = [
 			},
 			{
 				title: 'Puente del Vado Lancero (Puente del Balancero)',
-				image: '@assets/images/puente-vado-lancero.webp',
-				maps: ['https://goo.gl/maps/V3djyGQDaGuzckzV7'],
+				image: '/images/puente-vado-lancero.webp',
+				maps: [ 'https://goo.gl/maps/V3djyGQDaGuzckzV7'],
 				body: 'Construido entre 1797 y 1799,  se encuentra en dirección a Alcázar de San Juan, en las cercanías de los antiguos ' +
 					'molinos de pólvora del rey. Está compuesto por dos arcos de medio punto de piedra caliza, enmarcados por estribos ' +
 					'cilíndricos adosados a la fábrica. Es el único puente de dos ojos que se mantiene en pie del Canal del Gran ' +
@@ -22,8 +22,8 @@ export const PLACES = [
 			},
 			{
 				title: 'Castillo de Cervera (Casa grande)',
-				image: '@assets/images/casa-grande.webp',
-				maps: ['https://goo.gl/maps/t1iNuAtthwRDSQ1v6'],
+				image: '/images/casa-grande.webp',
+				maps: [ 'https://goo.gl/maps/t1iNuAtthwRDSQ1v6'],
 				body: 'Esta casa manchega de labranza consta de un patio delantero principal, donde está situado un palomar al estilo ' +
 					'de una almena de castillo de donde recibe la casa el nombre de Castillo de Cervera.<br/><br/> La vivienda principal ' +
 					'está situada en la parte de arriba donde vivían los propietarios, la parte de abajo delantera era donde vivían las ' +
@@ -40,7 +40,7 @@ export const PLACES = [
 			// },
 			{
 				title: 'Bombos manchegos',
-				image: '@assets/images/bombo.webp',
+				image: '/images/bombo.webp',
 				maps: ['https://goo.gl/maps/tdUNFWYxPF1MVXZk7', 'https://goo.gl/maps/hsnc56RKPqBXoAcR7', 'https://goo.gl/maps/Q4VzuQTcfvgTKu1SA'],
 				body: 'Son unas construcciones rural tradicionales de esta zona de La Mancha, utilizada como refugio para ' +
 					'pastores y labradores durante las faenas agrícolas y pastoriles. Es una estructura circular o elíptica, ' +
@@ -55,8 +55,8 @@ export const PLACES = [
 		places: [
 			{
 				title: 'Parque de D. Ramón Fernández Urrutia',
-				image: '@assets/images/parque.webp',
-				maps: ['https://goo.gl/maps/TqhojU7CVaypS3jj8'],
+				image: '/images/parque.webp',
+				maps: [ 'https://goo.gl/maps/TqhojU7CVaypS3jj8'],
 				web: null,
 				body: 'Es el parque más grande, ubicado a la entrada de la localidad. En su entrada se ' +
 					'encuentra el puente del Gran Prior y dentro del parque, la piscina municipal y su terraza. Dispone de un área ' +
@@ -64,7 +64,7 @@ export const PLACES = [
 			},
 			{
 				title: 'Parque de D. Juan de Villanueva',
-				maps: ['https://goo.gl/maps/sDJDJniugzpH2oj76'],
+				maps: [ 'https://goo.gl/maps/sDJDJniugzpH2oj76'],
 				body: 'Parque infantil ubicado detrás del colegio de la localidad. Dispone de columpios, balancín, varios bancos y ' +
 					'una fuente para beber agua. Está abierto las 24 horas.'
 			}
@@ -75,24 +75,24 @@ export const PLACES = [
 		places: [
 			{
 				title: 'C.E.I.P. Juan de Villanueva (C.R.A. Alciares)',
-				image: '@assets/images/colegio.webp',
-				maps: ['https://goo.gl/maps/A3aZWG8oYDNvyTwH9'],
+				image: '/images/colegio.webp',
+				maps: [ 'https://goo.gl/maps/A3aZWG8oYDNvyTwH9'],
 				web: 'https://craalciares.blogspot.com',
 				body: 'El colegio de la localidad, ubicado en el centro del pueblo. Forma parte de Colegio Rural Agrupado Alciares, ' +
 					'junto con las secciones de Arenales de San Gregorio y Cinco Casas. Dispone de un campo de fútbol/baloncesto, ' +
 					'ubicado detrás del colegio junto al parque D. Juan de Villanueva.'
 			}, {
 				title: 'Parada de autobuses',
-				image: '@assets/images/parada-bus.webp',
-				maps: ['https://goo.gl/maps/vpz9ZPW6TDoZhtoh7'],
+				image: '/images/parada-bus.webp',
+				maps: [ 'https://goo.gl/maps/vpz9ZPW6TDoZhtoh7'],
 				web: 'https://www.interbus.es',
 				body: 'Dispone de autobuses diarios a Alcázar de San Juan y Tomelloso.<br/> Horarios (se recomienda consultar en' +
 					' interbus):<br/> <br/> Alcazar - Alameda: L-V 12:00, S-D 12:10<br/> Alameda - Alcazar: L-D 14:25<br/> Tomelloso' +
 					' - Alameda: L-D 14:10<br/> Alameda - Tomelloso: L-V 12:15, S-D 12:30'
 			}, {
 				title: 'Consultorio Médico Local',
-				image: '@assets/images/consultorio.webp',
-				maps: ['https://goo.gl/maps/bRL1BRY6APQqwoj47'],
+				image: '/images/consultorio.webp',
+				maps: [ 'https://goo.gl/maps/bRL1BRY6APQqwoj47'],
 				body: 'Centro médico de atención primaria.'
 			}
 		]
