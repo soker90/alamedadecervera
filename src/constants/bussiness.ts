fdb7cdd66b3c088556af64545bb8fc7e07667720
Eduardo Parra <eduparra90@gmail.com>
2023-06-13 00:34:03 +0200

    new corpus and gallery

--- src/constants/bussiness.ts
+++ src/constants/bussiness.ts
@@ -6,12 +6,13 @@ export const BUSINESS = [
 				image: '/images/business/cooperativa.webp',
 				maps: ['https://goo.gl/maps/RG9pYFMjMQU69E2n7'],
 				web: 'https://www.coopsanlorenzo.es',
+				tel: ['926 58 20 42', '926 58 20 48'],
 				body:
-					'La Cooperativa San Lorenzo se fundó el año 1.954 en la localidad de Alameda de Cervera, provincia' +
+					'La Cooperativa San Lorenzo se fundó el año 1954 en la localidad de Alameda de Cervera, provincia' +
 					' de Ciudad Real y amparada en el marco de la Denominación de Origen La Mancha. Cuenta con 130 ' +
 					'socios que cultivan más de 800 hectáreas de viñedo comprendidas entre las variedades de uva Airén, ' +
 					'Macabeo, Verdejo, Chardonnay, Tempranillo, Garnacha, Bobal y Cabernet Sauvignon, de las que consiguen' +
-					' una producción media anual ed 8.000.000 de kilos de uva de los que un 80% son variedades blancas, ' +
+					' una producción media anual de 8.000.000 de kilos de uva, de los que un 80% son variedades blancas, ' +
 					'y el 20% restante tintas.'
 			}
 		]
