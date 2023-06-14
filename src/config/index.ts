de5451c48f46279c86e98ef39a0927a9d2719aa3
Eduardo Parra <eduparra90@gmail.com>
2023-06-14 18:11:57 +0200

    add twitter and remove instagram

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,6 @@ export * from './menu'
 export const RSS = {
 	TELEGRAM: 'https://t.me/alamedadecervera',
 	INSTAGRAM: 'https://www.instagram.com/alamedadecerveracom/',
-	FACEBOOK: 'https://www.facebook.com/alamedadecerveracom'
+	FACEBOOK: 'https://www.facebook.com/alamedadecerveracom',
+	TWITTER: 'https://twitter.com/alamedacervera'
 }
