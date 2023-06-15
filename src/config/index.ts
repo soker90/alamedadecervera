a23fc7df20b057cb931624fdeee15f6e11d8dacb
Eduardo Parra <eduardo.parramazuecos@telefonica.com>
2023-06-15 17:48:10 +0200

    Add instagram

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ export * from './menu'
 
 export const RSS = {
 	TELEGRAM: 'https://t.me/alamedadecervera',
-	INSTAGRAM: 'https://www.instagram.com/alamedadecerveracom/',
+	INSTAGRAM: 'https://www.instagram.com/alamedacerveracom/',
 	FACEBOOK: 'https://www.facebook.com/alamedadecerveracom',
 	TWITTER: 'https://twitter.com/alamedacervera'
 }
