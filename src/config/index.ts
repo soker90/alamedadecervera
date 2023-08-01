63a5cfe7801d3a049ba1a8825ad0dd3336dc0e3f
Eduardo Parra <eduparra90@gmail.com>
2023-08-01 22:32:39 +0200

    add mail

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,6 @@ export const RSS = {
 	TELEGRAM: 'https://t.me/alamedadecervera',
 	INSTAGRAM: 'https://www.instagram.com/alamedacerveracom/',
 	FACEBOOK: 'https://www.facebook.com/alamedadecerveracom',
-	TWITTER: 'https://twitter.com/alamedacervera'
+	TWITTER: 'https://twitter.com/alamedacervera',
+	MAIL: 'mailto:info@alamedadecervera.com'
 }
