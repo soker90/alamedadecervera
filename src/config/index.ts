7c4a73312addabbd63599a6e0f30b1ace369ad49
Eduardo Parra <eduparra90@gmail.com>
2023-09-26 20:25:19 +0200

    update twitter icon

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,6 @@ export const RSS = {
 	TELEGRAM: 'https://t.me/alamedadecervera',
 	INSTAGRAM: 'https://www.instagram.com/alamedacerveracom/',
 	FACEBOOK: 'https://www.facebook.com/alamedadecerveracom',
-	TWITTER: 'https://twitter.com/alamedacervera',
+	TWITTER: 'https://x.com/alamedacervera',
 	MAIL: 'mailto:info@alamedadecervera.com'
 }
