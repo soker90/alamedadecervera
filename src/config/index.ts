26e2fccf5bb11974ebefac55cc5af505ceaa32bf
Eduardo Parra <eduparra90@gmail.com>
2023-11-07 18:02:08 +0100

    add whatsapp

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ export * from './menu'
 
 export const RSS = {
 	TELEGRAM: 'https://t.me/alamedadecervera',
+	WHATSAPP: 'https://whatsapp.com/channel/0029VaE4sdA59PwM6nMCZC3x',
 	INSTAGRAM: 'https://www.instagram.com/alamedacerveracom/',
 	FACEBOOK: 'https://www.facebook.com/alamedadecerveracom',
 	TWITTER: 'https://x.com/alamedacervera',
