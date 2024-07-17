50da72317120ccf3c2c60e8621a343a54163b070
Eduardo Parra <eduparra90@gmail.com>
2024-07-17 19:16:49 +0200

    change animation news card

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-import animations from '@midudev/tailwind-animations'
-
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -25,5 +23,5 @@ export default {
 			}
 		}
 	},
-	plugins: [animations]
+	plugins: []
 }
