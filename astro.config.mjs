aa9b2be2ceeb242accbc2f8121ef4c2f53ec9698
Eduardo Parra <eduardo.parramazuecos@telefonica.com>
2025-07-16 16:22:58 +0200

    Solucionar problemas de build: actualizar importación de Vercel y añadir soporte para TailwindCSS 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 
-import vercel from '@astrojs/vercel/static'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +13,7 @@ export default defineConfig({
 	integrations: [preact(), tailwind(), mdx(), sitemap()],
 	output: 'static',
 	adapter: vercel({
+		mode: 'static',
 		imageConfig: {
 			sizes: [320, 640, 1280]
 		},
